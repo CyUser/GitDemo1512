e9adb2e49fc419e24ac510fef10b972ab259d397
zhangyi <1115144299@qq.com>
2018-01-23 16:00:08 +0800

    delete import

--- app/src/main/java/net/bwie/git/MainActivity.java
+++ app/src/main/java/net/bwie/git/MainActivity.java
@@ -2,8 +2,6 @@ package net.bwie.git;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 /**
